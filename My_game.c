4f4241c7f7c987c5c53ac0d269370556175a215f
ethan-tayar <ethan.tayar@gmail.com>
2020-09-23 19:39:37 +0300

    First commit +add of 2 fonction (The first commit is was made by error)

--- My_game.c
+++ My_game.c
@@ -20,13 +20,13 @@ int main(int argc, char *argv[])
     printf("But firs choose your sword you can choose: Samehada Damocles ");
     printf("Level : %d Here is your first choice:", level);
     readline(weapon, 20);
-    reajustpow(weapon, power);
+    reajustpow(weapon[20], power);
     printf("%d", power);
     printf("Who you want to confront? press 1 Artemis the goddes of the wild or press 2 for Kratos the god of war\n");
 }
-int reajustpow(char str[20], int power)
+void reajustpow(char str[20], int power)
 {
-    char *choice1[20];
+    char choice1[20];
     if (str == choice1)
     {
         power = 100;
